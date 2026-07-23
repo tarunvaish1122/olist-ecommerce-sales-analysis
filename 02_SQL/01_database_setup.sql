c92e3377905a524d995ab437c73c0aa7bb06a663
Tarun Vaishnav <tarunvaish1122@gmail.com>
2026-07-23 13:06:25 +0530

    Create 01_database_setup.sql

---
+++ 02_SQL/01_database_setup.sql
@@ -0,0 +1,22 @@
+-- =====================================================
+-- OLIST E-COMMERCE SALES ANALYSIS
+-- Database Creation
+-- =====================================================
+
+CREATE DATABASE olist;
+
+USE olist;
+
+-- Tables imported using MySQL Import Wizard
+
+-- customers
+-- geolocation
+-- orders
+-- order_items
+-- order_payments
+-- order_reviews
+-- products
+-- sellers
+-- product_category_name_translation
+
+-- Data cleaning was performed in Python before analysis.
