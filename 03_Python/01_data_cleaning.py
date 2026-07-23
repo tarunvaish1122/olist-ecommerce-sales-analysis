db7bbfed253df26cc4bdb8ffcc5e6349f63d5857
Tarun Vaishnav <tarunvaish1122@gmail.com>
2026-07-23 13:13:22 +0530

    Add data cleaning script

---
+++ 03_Python/01_data_cleaning.py
@@ -0,0 +1,15 @@
+"""
+Olist E-commerce Sales Analysis
+
+Data Cleaning
+"""
+
+# Data cleaning was performed using:
+# pandas
+# numpy
+
+# Tasks:
+# - Remove duplicates
+# - Handle missing values
+# - Convert date columns
+# - Export cleaned datasets
